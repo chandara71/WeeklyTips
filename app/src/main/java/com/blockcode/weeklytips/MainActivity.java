6e64966d32598721d05919fd59cfedf8092d91b9
Hengly Siev <hengly.s@pipay.com>
2024-03-22 02:03:58 +0700

    Communicate with Snackbar messages

--- app/src/main/java/com/blockcode/weeklytips/MainActivity.java
+++ app/src/main/java/com/blockcode/weeklytips/MainActivity.java
@@ -1,14 +1,85 @@
 package com.blockcode.weeklytips;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private TextView mLog;
+
+    private CoordinatorLayout mCoordinator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mLog = (TextView) findViewById(R.id.log);
+        mLog.setMovementMethod(new ScrollingMovementMethod());
+
+        mCoordinator = findViewById(R.id.coordinator_layout);
+
+    }
+
+    /**
+     * Run some code. If the TextView only displays the intro message, clear it first.
+     */
+    public void runCode(View view) {
+        if (mLog.getText().toString().equals(getString(R.string.intro_text))) {
+            mLog.setText("");
+        }
+        log("Running code");
+//        Toast.makeText(this, "This is a Toast message", Toast.LENGTH_LONG).show();
+        Snackbar.make(mCoordinator, "This is a snackbar message", Snackbar.LENGTH_LONG)
+                .setAction("Display Toast", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "This is a Toast message", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
+    }
+
+    /**
+     * Clear the output TextView
+     * @param view The button the user clicked
+     */
+    public void clearLog(View view) {
+        mLog.setText("");
+    }
+
+    /**
+     * Logs a message - called initially by runCode()
+     *
+     * @param message The message to display
+     */
+    private void log(String message) {
+        Log.i(TAG, message);
+        mLog.append(message + "\n");
+        adjustScroll();
+    }
+
+    /**
+     * Adjusts scroll vertically to ensure last line is displayed
+     */
+    private void adjustScroll() {
+        final int scrollAmount = mLog.getLayout()
+                .getLineTop(mLog.getLineCount()) - mLog.getHeight();
+        if (scrollAmount > 0)
+            mLog.scrollTo(0, scrollAmount);
+        else
+            mLog.scrollTo(0, 0);
     }
 }
